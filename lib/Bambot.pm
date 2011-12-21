48212306e11625e2cdfad72e687b9cceab2d623b
Brandon McCaig <bamccaig@gmail.com>
2011-12-20 21:49:43 -0500

    Fixing licensing ... ?

--- lib/Bambot.pm
+++ lib/Bambot.pm
@@ -17,6 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with bambot.  If not, see <http://www.gnu.org/licenses/>.
 # 
+
 use v5.010;
 use strict;
 use warnings;
